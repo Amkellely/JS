c426ee47ceb9c65bc1958cdc8bd5a39ad3c45978
Amkellely <108361555+Amkellely@users.noreply.github.com>
2023-03-14 00:17:02 +0300

    Задание с пушкой

--- java.js
+++ java.js
@@ -1,37 +1,44 @@
-// Получаем элементы canvas и angle
-const canvas = document.getElementById("canvas");
-const angleDiv = document.getElementById("angle");
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
 
-// Получаем контекст рисования для canvas
-const ctx = canvas.getContext("2d");
+// Начальные координаты ствола пушки
+var startX = 50;
+var startY = canvas.height - 50;
 
-// Рисуем прямоугольник-ствол пушки
-const gunWidth = 50;
-const gunHeight = 10;
-ctx.fillStyle = "gray";
-ctx.fillRect(canvas.width - gunWidth, canvas.height - gunHeight, gunWidth, gunHeight);
+// Размеры ствола пушки
+var width = 50;
+var height = 150;
 
-// Добавляем обработчик события "mousemove" на элемент canvas
+// Рисуем ствол пушки
+context.fillRect(startX, startY, width, height);
+
+// Функция для расчета угла между двумя точками
+function getAngle(x1, y1, x2, y2) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var angle = Math.atan2(dy, dx);
+    return angle;
+}
+
+// Обрабатываем событие движения мыши
 canvas.addEventListener("mousemove", function(event) {
-  // Вычисляем координаты курсора мыши внутри canvas
-  const rect = canvas.getBoundingClientRect();
-  const mouseX = event.clientX - rect.left;
-  const mouseY = event.clientY - rect.top;
+    // Координаты курсора мыши
+    var mouseX = event.clientX - canvas.offsetLeft;
+    var mouseY = event.clientY - canvas.offsetTop;
 
-  // Вычисляем угол между левым верхним углом стола пушки и курсором мыши
-  const gunX = canvas.width - gunWidth;
-  const gunY = canvas.height - gunHeight;
-  const angle = Math.atan2(mouseY - gunY, mouseX - gunX);
+    // Угол между левым верхним углом стола пушки и курсором мыши
+    var angle = getAngle(startX, startY, mouseX, mouseY);
 
-  // Поворачиваем canvas на вычисленный угол
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.save();
-  ctx.translate(gunX, gunY);
-  ctx.rotate(angle);
-  ctx.fillStyle = "gray";
-  ctx.fillRect(0, -gunHeight/2, gunWidth, gunHeight);
-  ctx.restore();
+    // Поворачиваем ствол пушки на расчитанный угол
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.translate(startX + width / 2, startY);
+    context.rotate(angle);
+    context.fillRect(-width / 2, 0, width, height);
+    context.restore();
 
-  // Выводим вычисленный угол в элемент angle
-  angleDiv.innerHTML = "Angle: " + (angle * 180 / Math.PI).toFixed(2) + " degrees";
+    // Выводим расчитанный угол в левом верхнем углу экрана
+    context.font = "bold 20px Arial";
+    context.fillStyle = "black";
+    context.fillText("Angle: " + angle.toFixed(2), 20, 40);
 });
