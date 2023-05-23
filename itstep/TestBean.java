80e2bfafa408fc2e95169b8c4b88e97adf6df1ae
Amkellely <108361555+Amkellely@users.noreply.github.com>
2023-05-23 20:55:18 +0300

    база

---
+++ itstep/TestBean.java
@@ -0,0 +1,28 @@
+package itstep;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(){
+        name ="Hello World";
+    }
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestBean{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
